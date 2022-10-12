35ab355dd2785c5c2ea455905ed0424cacd418a9
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-10-12 17:24:18 -0300

    Create safe area

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import SafeViewAndroid from "./src/styles/SafeViewAndroid";
+
+const readings = [
+	{
+		id: 1,
+		created_at: "2022-01-01 15:00:00",
+		cubic_meters: 50,
+		cubic_meter_price: 5,
+		conversion_coefficient: 2.4,
+	},
+	{
+		id: 2,
+		created_at: "2022-01-01 15:00:00",
+		cubic_meters: 50,
+		cubic_meter_price: 5,
+		conversion_coefficient: 2.4,
+	},
+	{
+		id: 3,
+		created_at: "2022-01-01 15:00:00",
+		cubic_meters: 50,
+		cubic_meter_price: 5,
+		conversion_coefficient: 2.4,
+	},
+	{
+		id: 4,
+		created_at: "2022-01-01 15:00:00",
+		cubic_meters: 50,
+		cubic_meter_price: 5,
+		conversion_coefficient: 2.4,
+	},
+];
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+			<ScrollView>
+				<StatusBar style="auto" />
+				<Text>Hi</Text>
+				<Text>Hi</Text>
+				<Text>Hi</Text>
+				<Text>Hi</Text>
+				<Text>Hi</Text>
+			</ScrollView>
+		</SafeAreaView>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
