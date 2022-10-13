8fdb2cfd05903375dbffdd4427f9e42b39cd8dee
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-10-13 07:31:27 -0300

    Start to create calculator

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+	SafeAreaView,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+} from "react-native";
 import ReadingList from "./src/components/Readings/ReadingList";
 import SafeViewAndroid from "./src/styles/SafeViewAndroid";
 
@@ -10,7 +17,12 @@ export default function App() {
 		>
 			<StatusBar />
 			<View>
-				<ReadingList />
+				<Text>Digite ou selecione leituras</Text>
+				<Text>1.</Text>
+				<TextInput style={styles.input} />
+				<Text>2.</Text>
+				<TextInput style={styles.input} />
+				<Text>Resultado</Text>
 			</View>
 		</SafeAreaView>
 	);
@@ -18,6 +30,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
 	defaultBackgroundColor: {
-		backgroundColor: "#EEE",
+		backgroundColor: "#aaa",
+	},
+	input: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		padding: 10,
 	},
 });
