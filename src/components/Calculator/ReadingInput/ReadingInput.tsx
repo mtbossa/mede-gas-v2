6c826f2618c25874962ecfe676611a1067eabd66
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-10-13 12:48:17 -0300

    Refactor

--- src/components/Calculator/ReadingInput/ReadingInput.tsx
+++ src/components/Calculator/ReadingInput/ReadingInput.tsx
@@ -11,20 +11,17 @@ function ReadingInput() {
 			style={styles.input}
 			value={reading}
 			maxLength={10}
+			placeholder="00000,000"
 			onChangeText={inputText => {
 				inputText = inputText.replace(/[^0-9,]/g, "");
 				if ((inputText.match(new RegExp(",", "g")) || []).length > 1) return;
 				if (inputText.length > 9) return;
+				if (inputText.includes(",")) {
+					const [_, decimals] = inputText.split(",");
+					if (decimals.length > 3) return;
+				}
 				if (inputText.length > 5 && !inputText.includes(",")) {
 					inputText = inputText.slice(0, 5) + "," + inputText.slice(5);
-				}
-				if (inputText.includes(",")) {
-					console.log("here"); //logs 4
-
-					const [int, decimals] = inputText.split(",");
-					if (decimals.length > 3) {
-						return;
-					}
 				}
 				setReading(inputText);
 			}}
