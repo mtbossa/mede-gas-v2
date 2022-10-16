91427e2c3f9b619d42353cb8266f157b96edfcc3
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-10-16 16:20:25 -0300

    Update AppSelect

--- src/components/Shared/AppSelect/AppSelect.tsx
+++ src/components/Shared/AppSelect/AppSelect.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Picker, PickerProps } from "@react-native-picker/picker";
+import { StyleSheet, View } from "react-native";
 
 function AppSelect<T>({ children, ...pickerProps }: PickerProps<T>) {
-	return <Picker {...pickerProps}>{children}</Picker>;
+	const [isFocused, setIsFocused] = useState(false);
+	return (
+		<View style={[styles.input, isFocused && styles.focused]}>
+			<Picker
+				onFocus={() => setIsFocused(true)}
+				onBlur={() => setIsFocused(false)}
+				{...pickerProps}
+			>
+				{children}
+			</Picker>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	focused: {
+		borderColor: "#125ee0",
+		borderWidth: 2,
+		outlineStyle: "solid",
+		outlineWidth: 4,
+	},
+	input: {
+		borderRadius: 3,
+		elevation: 2,
+		backgroundColor: "#FFFFF3",
+	},
+});
 
 export default AppSelect;
