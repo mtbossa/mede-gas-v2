b63f1b6489cfe109057a783af441459c3b3c4b9b
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-10-20 16:59:48 -0300

    Improve helper message

--- src/components/Calculator/TextHelpers/PriceHelper/PriceHelper.tsx
+++ src/components/Calculator/TextHelpers/PriceHelper/PriceHelper.tsx
@@ -5,8 +5,10 @@ import { colors } from "../../../../styles/colors";
 export default function PriceHelper() {
 	return (
 		<Text>
-			Esse é o valor que você paga por 1kg (quilo) de gás. Para obter esse
-			valor, encontre em contato com seu provedor de gás.
+			Esse é o valor que você paga por{" "}
+			<Text style={{ color: colors.subText }}>1 kg (quilo)</Text> de gás
+			utilizado. Para obter esse valor, encontre em contato com seu provedor de
+			gás.
 		</Text>
 	);
 }
